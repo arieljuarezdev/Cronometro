854b9bf91765d9df51849a30fc184f48ad17b67c
Ariel Juarez <85803503+arieljuarezdev@users.noreply.github.com>
2021-08-14 20:32:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+//Variable Area
+
+var minute = 0;
+var second = 0;
+var tens = 0;
+var inter;
+
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let reset = document.getElementById("reset");
+
+let mnt = document.getElementById("minute");
+let scd = document.getElementById("second");
+let tns = document.getElementById("tens");
+
+// Events
+play.onclick = iniCount;
+pause.onclick = pauCount;
+reset.onclick = resCount;
+
+
+// functions
+function iniCount(){
+    clearInterval(inter);
+    inter = setInterval(clock, 10);
+}
+
+function pauCount(){
+    clearInterval(inter);
+}
+
+function resCount(){
+    clearInterval(inter);
+    minute = 0;
+    second = 0;
+    tens = 0;
+
+
+    tns. innerHTML = "00";
+    scd. innerHTML = "00:";
+    mnt. innerHTML = "00:";
+}
+
+
+function clock(){
+    tens++;
+
+    if(tens <= 9){
+        tns.innerHTML = "0" + tens ;
+    }
+
+
+    if (tens > 9) {
+        tns.innerHTML = tens;
+    } 
+
+    if (tens > 99) {
+        second++
+        scd.innerHTML = "0" + second + ":";
+        tens = 0 
+        tns.innerHTML = "0" + tens; 
+    }
+
+    if (second > 9){
+        scd.innerHTML = second + ":";
+    }
+
+    if (second > 59){
+        minute++
+        mnt.innerHTML = "0" + minute + ":";
+        second = 0
+    }
+
+
+    
+    console.log("second")
+}
+    
+
+
+
+
